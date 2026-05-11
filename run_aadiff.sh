2a5912ba6d394d572b3219a3b6e0f1984c3ed9fc
baoqiwen <baoqiwen@baidu.com>
2026-05-11 17:26:23 +0800

    Add fa4 varlen md5 test.

--- run_aadiff.sh
+++ run_aadiff.sh
@@ -5,6 +5,15 @@ export FLAGS_check_cuda_error=1
 python -m pytest -v test_fwd_md5sum.py 2>&1 | tee test.log
 # python -m pytest -v test_bwd_md5sum.py 2>&1 | tee test.log
 
+# export FLAGS_cudnn_deterministic=1
+
 # run this if you want to update gt
 # python test_fwd_md5sum.py
 # python test_bwd_md5sum.py
+
+# if you update flash attention varlen
+# python -m pytest -v test_fwd_varlen_md5sum.py 2>&1 | tee test.log
+# python -m pytest -v test_bwd_varlen_md5sum.py 2>&1 | tee test.log
+# run this if you want to update gt
+# python test_fwd_varlen_md5sum.py
+# python test_bwd_varlen_md5sum.py
